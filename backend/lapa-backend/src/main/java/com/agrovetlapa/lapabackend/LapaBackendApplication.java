431910de881a2a5cc0581d34d105dd7f2758659c
Arthur Dantas <arthurdantaspro@gmail.com>
2022-12-14 11:08:39 -0300

    Criado projeto Spring

---
+++ backend/lapa-backend/src/main/java/com/agrovetlapa/lapabackend/LapaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agrovetlapa.lapabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LapaBackendApplication.class, args);
+    }
+
+}
---
+++ backend/lapa-backend/src/test/java/com/agrovetlapa/lapabackend/LapaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrovetlapa.lapabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
