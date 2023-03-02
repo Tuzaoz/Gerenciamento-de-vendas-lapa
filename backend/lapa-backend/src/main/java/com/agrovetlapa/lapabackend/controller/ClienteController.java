6e2df36b45422e2e74d7039505cf557e0c2b8aea
Arthur Dantas <arthurdantaspro@gmail.com>
2023-03-02 16:02:06 -0300

    Adicionada requisição para adicionar Clientes no ClienteController

--- backend/lapa-backend/src/main/java/com/agrovetlapa/lapabackend/controller/ClienteController.java
+++ backend/lapa-backend/src/main/java/com/agrovetlapa/lapabackend/controller/ClienteController.java
@@ -1,14 +1,14 @@
 package com.agrovetlapa.lapabackend.controller;
 
 import com.agrovetlapa.lapabackend.entities.Cliente;
+import com.agrovetlapa.lapabackend.entities.Venda;
 import com.agrovetlapa.lapabackend.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +27,12 @@ public class ClienteController {
         List<Cliente> clienteList = clienteService.findAll();
         return ResponseEntity.ok().body(clienteList);
 
+    }
+    @PostMapping
+    public ResponseEntity<Cliente> insertCliente(@RequestBody Cliente cliente) {
+        cliente = clienteService.insert(cliente);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId()).toUri();
+        return ResponseEntity.created(uri).body(cliente);
     }
 
 }
